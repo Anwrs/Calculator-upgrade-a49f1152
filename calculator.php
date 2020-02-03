3a399fd70ab470c92342bc756feb9bd48e14d3fe
Anouar ElGhazaoui <2080049@talnet.nl>
2020-02-03 10:42:47 +0100

    done

--- calculator.php
+++ calculator.php
@@ -1,27 +1,61 @@
 <?php 
 
-echo ("Welke operatie wil je uitvoeren? (+,-,%)"). PHP_EOL;
-$som = readline("");
-echo ("Eerste getal"). PHP_EOL;
-$a = is_numeric(readline(""));
-echo ("Tweede getal"). PHP_EOL;
-$b = is_numeric(readline(""));
+echo "Welke operatie wil je uitvoeren? (+,-,%)" . PHP_EOL;
+$vraag = readline("");
 
+if ($vraag === "+"){
+    
+    $som = "+";
+
+} else if ($vraag === "-"){
+    
+    $som = "+";
+
+} else if ($vraag === "%"){
+    
+    $som = "%";
+
+} else {
+
+    echo $vraag . " Is geen operator";
+    exit;
+
+}
+
+echo "Eerste getal" . PHP_EOL;
+$a = readline("");
+
+if (is_numeric($a)){
+
+} else {
+    echo  $a . " Is geen getal";
+    exit;
+}
+
+echo "Tweede getal" . PHP_EOL;
+$b = readline("");
+
+if (is_numeric($b)){ 
+
+} else {
+    echo  $b . " Is geen getal";
+    exit;
+}
 
 
 if($som === "+"){
 
     $ans = $a + $b;
-    echo  ("Uw antwoord is:"). $ans ;
+    echo  "Uw antwoord is: ". $ans ;
 
 } else if($som === "-"){
 
     $ans = $a - $b;
-    echo  ("Uw antwoord is: "). $ans ;
+    echo  "Uw antwoord is: ". $ans ;
 
 } else if($som === "%"){
 
     $ans = $a % $b;
-    echo ("Uw antwoord is: "). $ans ;
+    echo "Uw antwoord is: ". $ans ;
     
-}
+} 
